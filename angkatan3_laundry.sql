62d1f99d87fc5bd92229d0af54406f94ffa0ce2b
ozi16 <muhammadfahrurozi1603@gmail.com>
2024-11-15 16:04:32 +0700

    tambah database

--- angkatan3_laundry.sql
+++ angkatan3_laundry.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2024 at 02:44 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 15 Nov 2024 pada 10.03
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `level`
+-- Struktur dari tabel `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `customer_name` varchar(50) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `adress` text NOT NULL,
+  `creataed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `customer`
+--
+
+INSERT INTO `customer` (`id`, `customer_name`, `phone`, `adress`, `creataed_at`, `updated_at`) VALUES
+(2, 'yanto1', '987665', 'jl.mulyasari', '2024-11-15 04:25:40', '2024-11-15 04:28:07');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `level`
 --
 
 CREATE TABLE `level` (
@@ -35,7 +57,7 @@ CREATE TABLE `level` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `level`
+-- Dumping data untuk tabel `level`
 --
 
 INSERT INTO `level` (`id`, `nama_level`, `created_at`, `update_at`) VALUES
@@ -45,7 +67,64 @@ INSERT INTO `level` (`id`, `nama_level`, `created_at`, `update_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `trans_order`
+--
+
+CREATE TABLE `trans_order` (
+  `id` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `order_date` varchar(50) NOT NULL,
+  `order_status` tinyint(4) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `trans_order_detail`
+--
+
+CREATE TABLE `trans_order_detail` (
+  `id` int(11) NOT NULL,
+  `id_order` int(11) NOT NULL,
+  `id_service` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL,
+  `notes` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `type_of_service`
+--
+
+CREATE TABLE `type_of_service` (
+  `id` int(11) NOT NULL,
+  `service_name` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `type_of_service`
+--
+
+INSERT INTO `type_of_service` (`id`, `service_name`, `price`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'cuci dan gosok', 5000, 'cuci dan gosok sajas yaaa', '2024-11-15 06:37:23', '2024-11-15 06:37:23'),
+(2, 'cuci', 45000, 'cuci aja ya cokk...', '2024-11-15 06:37:56', '2024-11-15 06:37:56'),
+(3, 'gosok', 5000, 'gosok aja yee..', '2024-11-15 06:38:27', '2024-11-15 06:38:27'),
+(4, 'cuci besar', 7000, 'seperti selimut, mantel, sprei dll', '2024-11-15 06:39:38', '2024-11-15 06:41:41');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -60,55 +139,104 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`id`, `id_level`, `nama`, `email`, `password`, `username`, `created_at`, `updated_at`) VALUES
 (1, 1, 'Admin', 'admin@gmail.com', '12345678', 'admin', '2024-11-13 11:47:19', '2024-11-13 11:47:49'),
-(2, 2, 'Operator1', 'operator1@gmail.com', '12345678', 'operator1', '2024-11-13 13:25:58', '2024-11-13 13:25:58');
+(2, 2, 'Operator1', 'operator1@gmail.com', '12345678', 'operator1', '2024-11-13 13:25:58', '2024-11-13 13:25:58'),
+(3, 1, 'aoji', 'oji@GMAIL.COM', '123445', 'zio', '2024-11-15 02:45:21', '2024-11-15 02:45:21');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `level`
+-- Indeks untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `level`
 --
 ALTER TABLE `level`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `trans_order`
+--
+ALTER TABLE `trans_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `trans_order_detail`
+--
+ALTER TABLE `trans_order_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `type_of_service`
+--
+ALTER TABLE `type_of_service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_level` (`id_level`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `level`
+-- AUTO_INCREMENT untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `level`
 --
 ALTER TABLE `level`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `trans_order`
+--
+ALTER TABLE `trans_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `trans_order_detail`
+--
+ALTER TABLE `trans_order_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `type_of_service`
+--
+ALTER TABLE `type_of_service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `user`
+-- Ketidakleluasaan untuk tabel `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `level` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
